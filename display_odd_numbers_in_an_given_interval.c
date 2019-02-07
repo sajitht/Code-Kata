c0e42feafc24b12ad109347037de101248de7714
sajith <43330769+sajitht@users.noreply.github.com>
2019-02-07 14:13:26 +0530

    Create display_odd_numbers_in_an_given_interval.c

---
+++ display_odd_numbers_in_an_given_interval.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i,N1,N2;
+    scanf("%d %d",&N1,&N2);
+    for(i=N1+1;i<N2;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d\t",i);
+        }
+    }
+}
